e8c34c6a251f1c7f2527d693ac18281c2579c31c
DenisErygin <erygindenis93@gmail.com>
2023-11-22 12:19:29 +0300

    Create project

---
+++ AvitoTestMVVM/AvitoTestMVVM/Views/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AvitoTestMVVM
+//
+//  Created by Денис Ерыгин on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
